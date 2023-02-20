bf8899fec1e7279e0356a352033193d99873331d
Szántó Benjámin <61885011+vellt@users.noreply.github.com>
2023-02-20 18:28:05 +0100

    dark mode message

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f333cc1bdf5970ecc7de012a8028af73",
-"/": "f333cc1bdf5970ecc7de012a8028af73",
-"main.dart.js": "c5a917448aea959f25d86ac4dca56fe0",
+"index.html": "8f57092b673551d04ca04d2b6b6665c7",
+"/": "8f57092b673551d04ca04d2b6b6665c7",
+"main.dart.js": "8b01ebffd72580f6ba0050fcc950aaf9",
 "manifest.json": "67c01eee65e5dcb8de22c99edfee2d54",
 "version.json": "3288caaeaa7d6069a910dca7a8920d91"
 };
